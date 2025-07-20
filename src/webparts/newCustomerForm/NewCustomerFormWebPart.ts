3906ea498ced9efdc4ae1f82bbcf11ea9c9dbfd8
Guido Zambarda <gudkaronte+lavoro@gmail.com>
2025-07-20 18:10:35 +0200

    Added PnPjs to call Graph

--- src/webparts/newCustomerForm/NewCustomerFormWebPart.ts
+++ src/webparts/newCustomerForm/NewCustomerFormWebPart.ts
@@ -2,6 +2,8 @@ import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { graphfi, SPFx } from '@pnp/graph/presets/all';
+import '@pnp/graph/users';
 
 import NewCustomerForm from "./components/NewCustomerForm";
 import { INewCustomerFormProps } from './components/INewCustomerFormProps';
@@ -10,11 +12,24 @@ export interface INewCustomerFormWebPartProps {
 }
 
 export default class NewCustomerFormWebPart extends BaseClientSideWebPart<INewCustomerFormWebPartProps> {
+  private userFullName: string = '';
+
+  public async onInit(): Promise<void> {
+    await super.onInit();
+    try {
+      const graph = graphfi().using(SPFx(this.context));
+      const me = await graph.me();
+      this.userFullName = me.displayName || '';
+    } catch {
+      this.userFullName = this.context.pageContext.user.displayName || '';
+    }
+  }
+
   public render(): void {
     const element: React.ReactElement<INewCustomerFormProps> = React.createElement(
       NewCustomerForm,
       {
-        userDisplayName: this.context.pageContext.user.displayName
+        userDisplayName: this.userFullName
       }
     );
 
